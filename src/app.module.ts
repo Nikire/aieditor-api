2ea08c8b6e47b3470962684993c187832dc8e661
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2024-08-18 17:27:23 -0300

    added shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { DatabaseModule } from './modules/database/database.module';
 import { ProjectsModule } from './modules/projects/projects.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, ProjectsModule],
+  imports: [SharedModule, UsersModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
